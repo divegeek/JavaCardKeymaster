c8e5fcab9a05b0d5bb9e200de7bf0eb3d45a0b19
bvenkateswarlu <venki.manikanta@gmail.com>
2020-04-14 14:54:56 +0100

    Modified CborConverter class

--- HAL/keymaster/4.1/CborConverter.cpp
+++ HAL/keymaster/4.1/CborConverter.cpp
@@ -1,126 +1,150 @@
-/*
- **
- ** Copyright 2020, The Android Open Source Project
- **
- ** Licensed under the Apache License, Version 2.0 (the "License");
- ** you may not use this file except in compliance with the License.
- ** You may obtain a copy of the License at
- **
- **     http://www.apache.org/licenses/LICENSE-2.0
- **
- ** Unless required by applicable law or agreed to in writing, software
- ** distributed under the License is distributed on an "AS IS" BASIS,
- ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- ** See the License for the specific language governing permissions and
- ** limitations under the License.
- */
+#pragma
+#include <sstream>
+#include <iostream>
+#include <cppbor.h>
+#include <cppbor_parse.h>
+#include "CborConverter.h"
+using namespace cppbor;
 
-#include <CborConverter.h>
-
-namespace android {
-namespace hardware {
-namespace keymaster {
-namespace V4_1 {
-
-CborConverter::CborConverter() {
-//TODO
+HardwareAuthenticatorType convertToHardwareAuthenticatorType(uint64_t val) {
+    switch (static_cast<HardwareAuthenticatorType>(val)) {
+    case HardwareAuthenticatorType::NONE: 
+        return HardwareAuthenticatorType::NONE;
+    case HardwareAuthenticatorType::PASSWORD:
+        return HardwareAuthenticatorType::PASSWORD;
+    case HardwareAuthenticatorType::FINGERPRINT:
+        return HardwareAuthenticatorType::FINGERPRINT;
+    case HardwareAuthenticatorType::ANY:
+        return HardwareAuthenticatorType::ANY;
+    }
 }
 
-CborConverter::~CborConverter() {
-//TODO
+bool convertToTag(uint64_t val, TAG& tag) {
+    switch (static_cast<TAG>(val)) {
+    case TAG::ABC:
+        tag = TAG::ABC;
+        break;
+    case TAG::DEF:
+        tag = TAG::DEF;
+        break;
+    case TAG::GHI:
+        tag = TAG::GHI;
+        break;
+    default:
+        return false;
+    }
+    return true;
 }
 
-uint32_t CborConverter::decodeData(const uint8_t *buf, const size_t len, void **ctx) {
-    //TODO
+bool getTagValue(TAG& tag, KeyParameter& keyParam, uint64_t& value) {
+    return false;
 }
 
-uint32_t CborConverter::getElement(const void *ctx, const uint32_t index, void **innerCtx) {
-    //TODO
+bool CborConverter::getKeyparameter(const std::pair<const std::unique_ptr<Item>&,
+    const std::unique_ptr<Item>&> pair, KeyParameter& keyParam) {
+    bool ret = false;
+    KeyParameter::IntegerParams p;
+    uint64_t value;
+    //TAG will be always uint32_t
+    if (!getUint64<uint64_t>(pair.first, 0, value)) {
+        return ret;
+    }
+    if (!convertToTag(value, keyParam.tag)) return false;
+
+    if (MajorType::UINT == getType(pair.second)) {
+        if (!getUint64<uint64_t>(pair.second, 0, value)) {
+            return ret;
+        }
+        /* TODO*/
+        //Convert value to corresponding enum and assign to keyParam.f
+    }
+    else if (MajorType::BSTR == getType(pair.second)) {
+        if (!getBinaryArray(pair.second, 0, keyParam.blob)) {
+            return ret;
+        }
+    }
+    else {
+        return ret;
+    }
 }
 
-uint32_t CborConverter::getHmacSharingParameters(const void *ctx,
-                                                 std::vector<::android::hardware::keymaster::V4_0::HmacSharingParameters> &params) {
-    //TODO
+ParseResult CborConverter::decodeData(const std::vector<uint8_t> cborData) {
+    const uint8_t* pos;
+    std::unique_ptr<Item> item;
+    std::string message;
+    return parse(cborData);
 }
 
-uint32_t CborConverter::getVerificationTokens(const void *ctx,
-                                              std::vector<::android::hardware::keymaster::V4_0::VerificationToken> &tokens) {
-    //TODO
+bool CborConverter::getBinaryArray(const std::unique_ptr<Item>& item, const uint32_t pos, std::vector<uint8_t>& value) {
+    bool ret = false;
+    const std::unique_ptr<Item>& strItem = getItemAtPos(item, pos);
+    if ((strItem == nullptr) && (MajorType::BSTR != getType(strItem)))
+        return ret;
+
+    const Bstr* bstr = strItem.get()->asBstr();
+    for (auto bchar : bstr->value()) {
+        value.push_back(bchar);
+    }
+    ret = true;
+    return ret;
 }
 
-uint32_t CborConverter::getKeyParameters(const void *ctx,
-                                         std::vector<::android::hardware::keymaster::V4_0::KeyParameter> &params) {
-    //TODO
+
+bool CborConverter::getHmacSharingParameters(const std::unique_ptr<Item>& item, const uint32_t pos, HmacSharingParameters& params) {
+    std::vector<uint8_t> nonce;
+    bool ret = false;
+    //Seed
+    if (!getBinaryArray(item, pos, params.seed))
+        return ret;
+    //nonce
+    if (!getBinaryArray(item, pos+1, nonce))
+        return ret;
+    std::copy(nonce.begin(), nonce.end(), params.nonce);
+    ret = true;
+    return ret;
 }
 
-uint32_t CborConverter::getHardwareAuthTokens(const void *ctx,
-                                              std::vector<::android::hardware::keymaster::V4_0::HardwareAuthToken> &tokens) {
-    //TODO
+bool CborConverter::getHardwareAuthToken(const std::unique_ptr<Item>& item, const uint32_t pos, HardwareAuthToken& token) {
+    bool ret = false;
+    //challenge
+    if (!getUint64<uint64_t>(item, pos, token.challenge))
+        return ret;
+    //userId
+    if (!getUint64<uint64_t>(item, pos+1, token.userId))
+        return ret;
+    //AuthenticatorId
+    if (!getUint64<uint64_t>(item, pos+2, token.authenticatorId))
+        return ret;
+    //AuthType
+    uint64_t authType;
+    if (!getUint64<uint64_t>(item, pos+3, authType))
+        return ret;
+    token.authType = convertToHardwareAuthenticatorType(authType);
+    //Timestamp
+    if (!getUint64<uint64_t>(item, pos+4, token.timestamp))
+        return ret;
+    if (!getBinaryArray(item, pos + 5, token.mac))
+        return ret;
+    ret = true;
+    return ret;
 }
 
-uint32_t CborConverter::getKeyCharacteristics(const void *ctx,
-                                              std::vector<::android::hardware::keymaster::V4_0::KeyCharacteristics> &keyCharacteristics) {
-    //TODO
+bool CborConverter::getKeyParameters(const std::unique_ptr<Item>& item, const uint32_t pos, std::vector<KeyParameter> keyParams) {
+    bool ret = false;
+    const std::unique_ptr<Item>& mapItem = getItemAtPos(item, pos);
+    if ((mapItem == nullptr) && (MajorType::MAP != getType(mapItem)))
+        return ret;
+
+    const Map* map = mapItem.get()->asMap();
+    size_t mapSize = map->size();
+    for (int i = 0; i < mapSize; i++) {
+        KeyParameter param;
+        if (!getKeyparameter((*map)[i], param)) {
+            return ret;
+        }
+        keyParams.push_back(param);
+    }
+    ret = true;
+    return ret;
 }
 
-uint32_t CborConverter::getByteArray(const void *ctx, std::vector <uint8_t> &bytes) {
-    //TODO
-}
-
-uint32_t CborConverter::getUInt64Array(const void *ctx, std::vector <uint64_t> &values) {
-    //TODO
-}
-
-uint32_t CborConverter::getUInt32Array(const void *ctx, std::vector <uint32_t> &values) {
-    //TODO
-}
-
-uint32_t CborConverter::encodeHmacSharingParameters(
-        const std::vector<::android::hardware::keymaster::V4_0::HmacSharingParameters> &params,
-        std::vector <uint8_t> &cborData) {
-    //TODO
-}
-
-uint32_t CborConverter::encodeVerificationTokens(
-        const std::vector<::android::hardware::keymaster::V4_0::VerificationToken> &tokens,
-        std::vector <uint8_t> &cborData) {
-    //TODO
-}
-
-uint32_t CborConverter::encodeKeyParameters(
-        const std::vector<::android::hardware::keymaster::V4_0::KeyParameter> &params,
-        std::vector <uint8_t> &cborData) {
-    //TODO
-}
-
-uint32_t CborConverter::encodeHardwareAuthTokens(
-        const std::vector<::android::hardware::keymaster::V4_0::HardwareAuthToken> &tokens,
-        std::vector <uint8_t> &cborData) {
-    //TODO
-}
-
-uint32_t CborConverter::encodeKeyCharacteristics(
-        const std::vector<::android::hardware::keymaster::V4_0::KeyCharacteristics> &keyCharacteristics,
-        std::vector <uint8_t> &cborData) {
-    //TODO
-}
-
-uint32_t CborConverter::encodeByteArray(const std::vector <uint8_t> &bytes,
-                                        std::vector <uint8_t> &cborData) {
-    //TODO
-}
-
-uint32_t CborConverter::encodeUInt64Array(std::vector <uint64_t> &bytes,
-                                          std::vector <uint8_t> &cborData) {
-    //TODO
-}
-
-uint32_t CborConverter::encodeUInt32Array(std::vector <uint32_t> &bytes,
-                                          std::vector <uint8_t> &cborData) {
-    //TODO
-}
-
-} // namespace V4_1
-} // namespace keymaster
-} // namespace hardware
-} // namespace android
--- HAL/keymaster/include/CborConverter.h
+++ HAL/keymaster/include/CborConverter.h
@@ -1,186 +1,124 @@
-/*
- **
- ** Copyright 2020, The Android Open Source Project
- **
- ** Licensed under the Apache License, Version 2.0 (the "License");
- ** you may not use this file except in compliance with the License.
- ** You may obtain a copy of the License at
- **
- **     http://www.apache.org/licenses/LICENSE-2.0
- **
- ** Unless required by applicable law or agreed to in writing, software
- ** distributed under the License is distributed on an "AS IS" BASIS,
- ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- ** See the License for the specific language governing permissions and
- ** limitations under the License.
- */
+#pragma
+#ifndef __CBOR_CONVERTER_H_
+#define __CBOR_CONVERTER_H_
+#include <sstream>
+#include <iostream>
+#include <cstdint>
+#include <functional>
+#include <iterator>
+#include <memory>
+#include <numeric>
+#include "cppbor.h"
+#include "cppbor_parse.h"
+using namespace cppbor;
+/* Test Start */
+typedef struct HmacSharingParameters {
+    std::vector<uint8_t> seed;
+    uint8_t nonce[32];
+};
+enum class HardwareAuthenticatorType {
+    NONE = 0,
+    PASSWORD = 1 << 0,
+    FINGERPRINT = 1 << 1,
+    ANY = 0xFFFFFFFF
+};
+typedef struct HardwareAuthToken {
+    uint64_t challenge;
+    uint64_t userId;
+    uint64_t authenticatorId;
+    HardwareAuthenticatorType authType;
+    uint64_t timestamp;
+    std::vector<uint8_t> mac;
+};
+enum class Algorithm {
+    RSA = 0,
+    ECDSA = 1,
+    DSA = 2
+};
+enum class PaddingMode {
+    PKCS_1 = 0,
+    PKCS_5 = 1,
+    OAEP = 2,
+};
+enum class TAG {
+    ABC = 0,
+    DEF = 1,
+    GHI = 2
+};
+typedef struct KeyParameter {
+    TAG tag;
+    union IntegerParams {
+        Algorithm algorithm;
+        PaddingMode mode;
+        bool boolValue;
+        uint32_t u32t;
+        uint64_t u64t;
+    };
+    IntegerParams f;
+    std::vector<uint8_t> blob;
+};
+typedef struct VerificationToken {
+    uint64_t challenge;
+    uint64_t timestamp;
 
-#ifndef ANDROID_HARDWARE_KEYMASTER_V4_1_CBORCONVERTER_H
-#define ANDROID_HARDWARE_KEYMASTER_V4_1_CBORCONVERTER_H
+};
+/*Test End*/
+class CborConverter
+{
+public:
+	CborConverter() = default;
+	~CborConverter() = default;
 
-namespace android {
-namespace hardware {
-namespace keymaster {
-namespace V4_1 {
+	ParseResult decodeData(const std::vector<uint8_t> cborData);
 
-class CborConverter {
-    public:
-        CborConverter();
+    /* Use this function to get both signed and usinged integers.*/
+	template<typename T>
+    bool getUint64(const std::unique_ptr<Item>& item, const uint32_t pos, T& value);
+    bool getHmacSharingParameters(const std::unique_ptr<Item>& item, const uint32_t pos, HmacSharingParameters& params);
+    bool getBinaryArray(const std::unique_ptr<Item>& item, const uint32_t pos, std::vector<uint8_t>& vec);
+    bool getHardwareAuthToken(const std::unique_ptr<Item>& item, const uint32_t pos, HardwareAuthToken& authType);
+    bool getKeyParameters(const std::unique_ptr<Item>& item, const uint32_t pos, std::vector<KeyParameter> keyParams);
+   
 
-        virtual ~CborConverter();
+private:
+	inline MajorType getType(const std::unique_ptr<Item> &item) { return item.get()->type(); }
+    bool getKeyparameter(const std::pair<const std::unique_ptr<Item>&,
+        const std::unique_ptr<Item>&> pair, KeyParameter& keyParam);
+    inline const std::unique_ptr<Item>& getItemAtPos(const std::unique_ptr<Item>& item, const uint32_t pos) {
+        const Array* arr = nullptr;
 
-        //Conversion methods
-        /**
-         * Decodes the CBOR data and returns a context. This context
-         * has to be passed to each API for retrieving individual elements.
-         * @param[in] buf The array of bytes to decode.
-         * @param[in] len The number of bytes in the array.
-         * @param[out] ctx Data structure formed from parsed CBOR data.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t decodeData(const uint8_t *buf, const size_t len, void **ctx);
-
-        /**
-         * Get an element at index from the main context.
-         * @param[in] ctx Data structure pointer returned from @ref decodeData(const uint8_t, const size_t, void**).
-         * @param[in] index The position where to retrieve the element from.
-         * @param[out] innerCtx Data structure corresponding to the element at index.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t getElement(const void *ctx, const uint32_t index, void **innerCtx);
-
-        /**
-         * Retrieves the array of HmacSharingParameters.
-         * @param[in] ctx Data structure context corresponding to this element.
-         * @param[out] params The array of HmacSharingParameters.
-         * @return  0 if success, or error code if failure.
-         */
-        uint32_t getHmacSharingParameters(const void *ctx, std::vector<::android::hardware::keymaster::V4_0::HmacSharingParameters>& params);
-
-        /**
-         * Converts from array of HmacSharingParameters structures to Cbor format.
-         * @param[in] params Array of HmacSharingParameters instances to be encoded.
-         * @param[out] cborData Encoded byte array.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t encodeHmacSharingParameters(const std::vector<::android::hardware::keymaster::V4_0::HmacSharingParameters>& params, std::vector<uint8_t>& cborData);
-
-        /**
-         * Retrieves the array of VerificationTokens.
-         * @param[in] ctx Data structure context corresponding to this element.
-         * @param[out] tokens The array of VerificationToken structures.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t getVerificationTokens(const void *ctx, std::vector<::android::hardware::keymaster::V4_0::VerificationToken>& tokens);
-
-        /**
-         * Converts from array of VerificationToken structures to Cbor format.
-         * @param[in] tokens Array of VerificationToken instances to be encoded.
-         * @param[out] cborData Encoded byte array.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t encodeVerificationTokens(const std::vector<::android::hardware::keymaster::V4_0::VerificationToken>& tokens, std::vector<uint8_t>& cborData);
-
-        /**
-         * Retrieves the array of KeyParameter structures.
-         * @param[in] ctx Data structure context corresponding to this element.
-         * @param[out] params The array of KeyParameter structures.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t getKeyParameters(const void *ctx, std::vector<::android::hardware::keymaster::V4_0::KeyParameter>& params);
-
-        /**
-         * Converts from array of KeyParameter structures to Cbor format.
-         * @param[in] params Array of KeyParameter instances to be encoded.
-         * @param[out] cborData Encoded byte array.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t encodeKeyParameters(const std::vector<::android::hardware::keymaster::V4_0::KeyParameter>& params, std::vector<uint8_t>& cborData);
-
-        /**
-         * Retrieves the array of HardwareAuthToken structures.
-         * @param[in] ctx Data structure context corresponding to this element.
-         * @param[out] tokens The array of HardwareAuthToken structures.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t getHardwareAuthTokens(const void *ctx, std::vector<::android::hardware::keymaster::V4_0::HardwareAuthToken>& tokens);
-
-        /**
-         * Converts from array of HardwareAuthToken structures to Cbor format.
-         * @param[in] tokens Array of HardwareAuthToken instances to be encoded.
-         * @param[out] cborData Encoded byte array.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t encodeHardwareAuthTokens(const std::vector<::android::hardware::keymaster::V4_0::HardwareAuthToken>& tokens, std::vector<uint8_t>& cborData);
-
-        /**
-         * Retrieves the array of keyCharacteristics structures.
-         * @param[in] ctx Data structure context corresponding to this element.
-         * @param[out] keyCharacteristics The array of keyCharacteristics structures.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t getKeyCharacteristics(const void *ctx, std::vector<::android::hardware::keymaster::V4_0::KeyCharacteristics>& keyCharacteristics);
-
-        /**
-         * Converts from array of KeyCharacteristics structures to Cbor format.
-         * @param[in] keyCharacteristics Array of KeyCharacteristics instances to be encoded.
-         * @param[out] cborData Encoded byte array.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t encodeKeyCharacteristics(const std::vector<::android::hardware::keymaster::V4_0::KeyCharacteristics>& keyCharacteristics, std::vector<uint8_t>& cborData);
-
-        /**
-         * Retrieves byte array.
-         * @param[in] ctx Data structure context corresponding to this element.
-         * @param[out] bytes The parsed byte array.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t getByteArray(const void *ctx, std::vector<uint8_t>& bytes);
-
-        /**
-         * Converts from vector array to Cbor format.
-         * @param[in] bytes Byte array instance to be encoded.
-         * @param[out] cborData Encoded byte array.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t encodeByteArray(const std::vector<uint8_t>& bytes, std::vector<uint8_t>& cborData);
-
-        /**
-         * Retrieves uint64_t array.
-         * @param[in] ctx Data structure context corresponding to this element.
-         * @param[out] values The parsed uint64_t array.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t getUInt64Array(const void *ctx, std::vector<uint64_t>& values);
-
-        /**
-         * Retrieves uint32_t array.
-         * @param[in]  ctx Data structure context corresponding to this element.
-         * @param[out] values The parsed uint32_t array.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t getUInt32Array(const void *ctx, std::vector<uint32_t>& values);
-
-        /**
-         * Converts from uint64_t array to Cbor format.
-         * @param bytes uint64_t array to be encoded.
-         * @param cborData Encoded byte array.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t encodeUInt64Array(std::vector<uint64_t> &bytes, std::vector<uint8_t>& cborData);
-
-        /**
-         * Converts from uint32_t array to Cbor format.
-         * @param bytes uint64_t array to be encoded.
-         * @param cborData Encoded byte array.
-         * @return 0 if success, or error code if failure.
-         */
-        uint32_t encodeUInt32Array(std::vector<uint32_t> &bytes, std::vector<uint8_t>& cborData);
+        if (MajorType::ARRAY != getType(item)) {
+            return nullptr;
+        }
+        arr = item.get()->asArray();
+        if (arr->size() < (pos + 1)) {
+            return nullptr;
+        }
+        return (*arr)[pos];
+    }
 };
 
-} // namespace V4_1
-} // namespace keymaster
-} // namespace hardware
-} // namespace android
+template<typename T>
+bool CborConverter::getUint64(const std::unique_ptr<Item>& item, const uint32_t pos, T& value) {
+    bool ret = false;
+    const std::unique_ptr<Item>& intItem = getItemAtPos(item, pos);
+    
+    if ((intItem == nullptr) ||
+        (std::is_unsigned<T>::value && (MajorType::UINT != getType(intItem))) ||
+        ((std::is_signed<T>::value && (MajorType::NINT != getType(intItem))))) {
+        return ret;
+    }
 
-#endif //ANDROID_HARDWARE_KEYMASTER_V4_1_CBORCONVERTER_H
+    if (std::is_unsigned<T>::value) {
+        const Uint* uintVal = intItem.get()->asUint();
+        value = uintVal->value();
+    }
+    else {
+        const Nint* nintVal = intItem.get()->asNint();
+        value = nintVal->value();
+    }
+    ret = true;
+    return ret; //success
+}
+
+#endif
