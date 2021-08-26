b6a5f1aa3d07a5470f104cbf92b56eacb476c898
Venkateswarlu Bhamidipati <kameswarasrisubrahmanya.bhamidipati@quest-global.com>
2021-08-26 20:33:22 +0000

    Fix for the issue that operation handle(i.e Challenge), inside the HardwareAuthToken and VerificationToken is mismatching with the operation handle generated by Strongbox.

--- HAL/keymaster/include/JavacardKeymaster4Device.h
+++ HAL/keymaster/include/JavacardKeymaster4Device.h
@@ -54,6 +54,15 @@ using ::android::hardware::keymaster::V4_0::Tag;
 
 using V41ErrorCode = ::android::hardware::keymaster::V4_1::ErrorCode;
 
+enum class OperationType {
+    /* Public operations are processed inside softkeymaster */
+    PUBLIC_OPERATION = 0,
+    /* Private operations are processed inside strongbox */
+    PRIVATE_OPERATION = 1,
+    UNKNOWN = 2,
+};
+
+
 class JavacardKeymaster4Device : public IKeymasterDevice {
   public:
   
@@ -87,8 +96,33 @@ class JavacardKeymaster4Device : public IKeymasterDevice {
 
 protected:
     CborConverter cborConverter_;
-	
-private:
+
+ private:
+    ErrorCode handleBeginPublicKeyOperation(
+            KeyPurpose purpose, const hidl_vec<uint8_t>& keyBlob,
+            const hidl_vec<KeyParameter>& inParams, hidl_vec<KeyParameter>& outParams,
+            uint64_t& operationHandle);
+
+    ErrorCode handleBeginPrivateKeyOperation(
+            KeyPurpose purpose, const hidl_vec<uint8_t>& keyBlob,
+            const hidl_vec<KeyParameter>& inParams,
+            const HardwareAuthToken& authToken, hidl_vec<KeyParameter>& outParams,
+            uint64_t& operationHandle);
+
+    ErrorCode handleBeginOperation(KeyPurpose purpose,
+                                   const hidl_vec<uint8_t>& keyBlob,
+                                   const hidl_vec<KeyParameter>& inParams,
+                                   const HardwareAuthToken& authToken,
+                                   hidl_vec<KeyParameter>& outParams,
+                                   uint64_t& operationHandle,
+                                   OperationType& operType);
+ 
+    ErrorCode abortOperation(uint64_t operationHandle, OperationType operType);
+
+    ErrorCode abortPublicKeyOperation(uint64_t operationHandle);
+
+    ErrorCode abortPrivateKeyOperation(uint64_t operationHandle);
+
     std::unique_ptr<::keymaster::AndroidKeymaster> softKm_;
     std::unique_ptr<OperationContext> oprCtx_;
     bool isEachSystemPropertySet;
