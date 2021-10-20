d7286d024beb4c8d48d2a32fead34b4f57d340a4
Subrahmanya Manikanta Venkateswarlu Bhamidipati Kameswara Sri <subrahmanyaman@google.com>
2021-10-20 17:05:22 +0000

    Added conversion to milliseconds

--- Applet/AndroidSEProvider/src/com/android/javacard/keymaster/KMAttestationCertImpl.java
+++ Applet/AndroidSEProvider/src/com/android/javacard/keymaster/KMAttestationCertImpl.java
@@ -84,7 +84,7 @@ public class KMAttestationCertImpl implements KMAttestationCert {
   private static final byte keyUsageKeyEncipher = (byte) 0x20; // 2nd- bit
   private static final byte keyUsageDataEncipher = (byte) 0x10; // 3rd- bit
 
-  rivate static final byte KEYMASTER_VERSION = 40;
+  private static final byte KEYMASTER_VERSION = 40;
   private static final byte ATTESTATION_VERSION = 3;
   private static final byte[] pubExponent = {0x01, 0x00, 0x01};
   private static final byte SERIAL_NUM = (byte) 0x01;
